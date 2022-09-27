aa04b975954bdadc761ba53a4324732846b5f403
myoostis <parkgin2011@naver.com>
2022-09-27 16:35:41 +0900

    02

--- main.c
+++ main.c
@@ -5,21 +5,18 @@
 
 int main(int argc, char *argv[]) {
 	
-	int x, y, z, m;
-	int a, b, c;
+	int x, y;
 	
-	x = 1;
-	y = 3;
-	z = 5;
+	printf("input two integers : ");
+	scanf("%i %i", &x, &y);
 	
-	a = 3;
-	b = 4;
-	c = 5;
+	printf("+ result is %i\n", x+y);
+	printf("- result is %i\n", x-y);
+	printf("* result is %i\n", x*y);
+	printf("/ result is %i\n", x/y);	
+	printf("%% result is %i\n", x%y);
 	
-	y = a*x*x + b*x + c;
-	m = (x + y+ z)/3;
-	
-	printf("y = %d, m = %d", y, m);
 	
 	return 0;
+	
 }
